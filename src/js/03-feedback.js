b93ccfe46b9cc4bbf6a6e7c5a63436ec41365e48
Olha-PSM <Olha84pismennaa@gmail.com>
2023-06-13 19:21:20 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,14 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
-const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', throttle(onFormInput, 500));
-form.addEventListener('submit', onFormSubmit);
+const formData = {};
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('.feedback-form input');
+const messageEl = document.querySelector('.feedback-form textarea');
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
 
 savedLocalStorageValue();
 
@@ -15,18 +18,16 @@ function onFormInput(evt) {
 }
 
 function onFormSubmit(evt) {
-  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   evt.preventDefault();
   evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
 function savedLocalStorageValue() {
-  const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  const { email, message } = form.elements;
-
-  if (data) {
-    email.value = data.email || '';
-    message.value = data.message || '';
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedMessage) {
+    emailEl.value = savedMessage.email || '';
+    messageEl.value = savedMessage.message || '';
   }
 }
