1b432ff9b001861b35972beaa7212cf14401cd8a
Olha-PSM <Olha84pismennaa@gmail.com>
2023-06-13 15:07:44 +0300

    second commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+savedLocalStorageValue();
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function savedLocalStorageValue() {
+  const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const { email, message } = form.elements;
+
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+}
